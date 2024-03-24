1c5df7e5c2f7f0e1fdf7abc0e9a27d5692db0985
GlassB3ad <j.lahtivirta@gmail.com>
2024-03-24 13:06:04 +0200

    refactored untestable

--- src/testable1.mjs
+++ src/testable1.mjs
@@ -1,10 +1,14 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
+export function daysBetween(startDate, endDate) {
+    const diffMillis = endDate.getTime() - startDate.getTime();
+    return Math.floor(diffMillis / millisPerDay) - 1;
+}
+
 export function daysUntilChristmas(today) {
     const christmasDay = new Date(today.getFullYear(), 12 - 1, 25);
     if (today.getTime() > christmasDay.getTime()) {
         christmasDay.setFullYear(now.getFullYear() + 1);
     }
-    const diffMillis = christmasDay.getTime() - today.getTime();
-    return Math.floor(diffMillis / millisPerDay) - 1;
+    return daysBetween(today, christmasDay)
 }
