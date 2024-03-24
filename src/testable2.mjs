3e1716d4a2984dd44b695c0c1041dd94fb21e3a2
GlassB3ad <j.lahtivirta@gmail.com>
2024-03-24 13:28:24 +0200

    refactored and tested untestable2

---
+++ src/testable2.mjs
@@ -0,0 +1,19 @@
+function diceRoll() {
+    const min = 1;
+    const max = 6;
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+export function rollDices() {
+    return [diceRoll(), diceRoll()]
+}
+
+export function diceHandValue(die1, die2) {
+    if (die1 === die2) {
+        // one pair
+        return 100 + die1;
+    } else {
+        // high die
+        return Math.max(die1, die2);
+    }
+}
--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -1,10 +1,9 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { diceHandValue } from "../src/untestable2.mjs";
+import { diceHandValue } from "../src/testable2.mjs";
 
 describe("Untestable 2: a dice game", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(diceHandValue()).to.be.a("number");
+  test("Hand of 4 and 5 dices has valuue 5", () => {
+    expect(diceHandValue(4, 5)).to.equal(5);
   });
 });
