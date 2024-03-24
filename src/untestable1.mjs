0073adce65e7b0b63eb095cde280ffead162dd9e
GlassB3ad <j.lahtivirta@gmail.com>
2024-03-24 13:02:33 +0200

    refactored untestable

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -1,5 +1,6 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
+//Hard to test because the execution of the code depends on time which varies between test runs.
 export function daysUntilChristmas() {
   const now = new Date();
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
@@ -10,3 +11,4 @@ export function daysUntilChristmas() {
   const diffMillis = christmasDay.getTime() - today.getTime();
   return Math.floor(diffMillis / millisPerDay);
 }
+
--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -1,10 +1,10 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { daysUntilChristmas } from "../src/untestable1.mjs";
+import { daysUntilChristmas } from "../src/testable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+  test("4 days to christmas from 20th december", () => {
+    const december20th = new Date(2024, 11, 20)
+    expect(daysUntilChristmas(december20th)).to.equal(4);
   });
 });
