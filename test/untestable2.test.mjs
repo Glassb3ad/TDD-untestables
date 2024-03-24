3ab2c4d428eeb10dabd095e870877c35067860f9
GlassB3ad <j.lahtivirta@gmail.com>
2024-03-24 13:29:40 +0200

    new test for  untestable2

--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -3,7 +3,11 @@ import { expect } from "chai";
 import { diceHandValue } from "../src/testable2.mjs";
 
 describe("Untestable 2: a dice game", () => {
-  test("Hand of 4 and 5 dices has valuue 5", () => {
+  test("Hand of 4 and 5 has value 5", () => {
     expect(diceHandValue(4, 5)).to.equal(5);
+  });
+
+  test("Hand of 6 and 2 has value 6", () => {
+    expect(diceHandValue(6, 2)).to.equal(6);
   });
 });
