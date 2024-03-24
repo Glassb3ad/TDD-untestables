82f411c7b8c29f17ad5e3a860d1794eac7037ff6
GlassB3ad <j.lahtivirta@gmail.com>
2024-03-24 13:40:43 +0200

    new test for random part of  untestable2

--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -1,6 +1,8 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { diceHandValue } from "../src/testable2.mjs";
+import { diceHandValue, rollDices } from "../src/testable2.mjs";
+
+const isDiceWithCorrectValue = die => die <= 6 || die >= 1
 
 describe("Untestable 2: a dice game", () => {
   test("Hand of 4 and 5 has value 5", () => {
@@ -10,4 +12,17 @@ describe("Untestable 2: a dice game", () => {
   test("Hand of 6 and 2 has value 6", () => {
     expect(diceHandValue(6, 2)).to.equal(6);
   });
+
+  test("Hand of 6 and 6 has value 106", () => {
+    expect(diceHandValue(6, 6)).to.equal(106);
+  });
+
+  test("Dice roll always returns dices with values between 1 and 6", () => {
+    let rolledDiceWithIncorrectValue = false
+    for (let i = 0; i < 100; i++) {
+      const [die1, die2] = rollDices()
+      if (!(isDiceWithCorrectValue(die1) && isDiceWithCorrectValue)(die2)) rolledDiceWithIncorrectValue = true
+    }
+    expect(rolledDiceWithIncorrectValue).to.be.false
+  })
 });
