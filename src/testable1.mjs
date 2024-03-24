9a787dce262f42112b2acbb3d1c930dea271fd63
GlassB3ad <j.lahtivirta@gmail.com>
2024-03-24 13:20:31 +0200

    added new test to  untestable1

--- src/testable1.mjs
+++ src/testable1.mjs
@@ -2,13 +2,13 @@ const millisPerDay = 24 * 60 * 60 * 1000;
 
 export function daysBetween(startDate, endDate) {
     const diffMillis = endDate.getTime() - startDate.getTime();
-    return Math.floor(diffMillis / millisPerDay) - 1;
+    return Math.floor(diffMillis / millisPerDay);
 }
 
 export function daysUntilChristmas(today) {
     const christmasDay = new Date(today.getFullYear(), 12 - 1, 25);
     if (today.getTime() > christmasDay.getTime()) {
-        christmasDay.setFullYear(now.getFullYear() + 1);
+        christmasDay.setFullYear(today.getFullYear() + 1);
     }
     return daysBetween(today, christmasDay)
 }
--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -5,6 +5,16 @@ import { daysUntilChristmas } from "../src/testable1.mjs";
 describe("Untestable 1: days until Christmas", () => {
   test("4 days to christmas from 20th december", () => {
     const december20th = new Date(2024, 11, 20)
-    expect(daysUntilChristmas(december20th)).to.equal(4);
+    expect(daysUntilChristmas(december20th)).to.equal(5);
+  });
+
+  test("More than 300 days to christmas from 26th december", () => {
+    const december26th = new Date(2024, 11, 26)
+    expect(daysUntilChristmas(december26th)).to.greaterThan(300);
+  });
+
+  test("0 days to christmas from 25th december", () => {
+    const december26th = new Date(2024, 11, 26)
+    expect(daysUntilChristmas(december26th)).to.greaterThan(0);
   });
 });
